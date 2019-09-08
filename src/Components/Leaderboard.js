ee72a8a7a83e525a3de8960862a3e4c65baa456c
Victoria Nguyen <nguyen.victoriais@gmail.com>
2019-09-07 21:57:44 -0500

    Added general layout of leaderboard using react-table and namor modules

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,106 +1,42 @@
-import TableRow from './TableRow.js';
-import ToSortHeader from './ToSortHeader.js';
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import {makeData} from './Data.js';
 import './Leaderboard.css';
-import React, {Component} from 'react';
-
-const url = `https://fcctop100.herokuapp.com/api/fccusers/top/`;
-
-const sortByMapping = [
-  {type: 'past30Days', heading: 'Past 30 Days'},
-  {type: 'allTime', heading: 'Total Points'},
-];
+import 'react-table/react-table.css';
 
 class Leaderboard extends Component {
-  constructor() {
+    constructor() {
     super();
-
     this.state = {
-      defaultData: [],
-      recentData: [],
-      allTimeData: [],
-      recentDataDesc: true,
-      allTimeDataDesc: false,
+      data: makeData()
     };
   }
-
-  urlGenerator(slug) {
-    return url + slug;
-  }
-
-  componentDidMount() {
-    this.fetchData(this.urlGenerator('recent'), 'recent');
-    this.fetchData(this.urlGenerator('alltime'), 'allTime');
-  }
-
-  fetchData(url, type) {
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      if (type === 'recent') {
-        this.setState({
-          defaultData: data,
-          recentData: data,
-        });
-      } else if (type === 'allTime') {
-        this.setState({
-          allTimeData: data,
-        });
-      }
-    })
-    .catch((err) => console.error('err::', err))
-  }
-
-  handleSort = (sortType) => {    
-    if (sortType === 'past30Days') {
-      this.sortRowsRecentData();
-    }
-
-    else if (sortType === 'allTime') {
-      this.sortAllTimeData();
-    }
-  }
-
-  sortRowsRecentData() {
-    const { recentData, recentDataDesc } = this.state;
-
-    const sortedRecentData = recentDataDesc ? recentData.sort((a, b) => a.recent - b.recent) : recentData.sort((a, b) => b.recent - a.recent);
-    this.setState({
-      defaultData: sortedRecentData,
-      recentDataDesc: !this.state.recentDataDesc,
-    });
-  }
-
-  sortAllTimeData() {
-    const { allTimeData, allTimeDataDesc } = this.state;
-
-    const sortedAllTimeData = allTimeDataDesc ? allTimeData.sort((a, b) => a.alltime - b.alltime) : allTimeData.sort((a, b) => b.alltime - a.alltime);
-    this.setState({
-      defaultData: sortedAllTimeData,
-      allTimeDataDesc: !this.state.allTimeDataDesc,
-    });
-  }
-
   render() {
-    const rows = this.state.defaultData.map((rowData, i) => {
-      return <TableRow key={i} id={i+1} rowData={rowData} />
-    });
-
-    const sorters = sortByMapping.map((toSortBy, i) => {
-      return <ToSortHeader key={i} sortBy={toSortBy.type} heading={toSortBy.heading} onClick={this.handleSort} />
-    });
-
+    const { data } = this.state;
     return (
-      <table>
-        <tbody>
-          <tr>
-            <th scope="col">#Rank</th>
-            <th scope="col">Username</th>
-            {sorters}
-          </tr>
-          {rows}
-          </tbody>
-      </table>
-    )
+      <div>
+        <ReactTable
+          data={data}
+          columns={[
+            {
+                Header: 'First Name',
+                accessor: 'firstName'
+            },
+            {
+                Header: 'Last Name',
+                accessor: 'lastName'
+            }
+              
+          ]}
+          defaultPageSize={20}
+          style={{
+            height: "400px" // This will force the table body to overflow and scroll, since there is not enough room
+          }}
+          className="-striped -highlight"
+        />
+        <br />
+      </div>
+    );
   }
 }
 
