fd0ba4cdb1649c745bdd1b07893f95715885d795
Victoria Nguyen <nguyen.victoriais@gmail.com>
2019-09-07 23:37:42 -0500

    Made website slightly more responsive but still needs a lot of work. Added sponsor to landing page.

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
       return (
          <div className="About-container">
            <p className="header">About</p>
-           <p className="content">UT Programming Contest (UTPC) is a chapter of the International Collegiate <br/> Programming Contest (ICPC).</p>
+           <p className="content">UT Programming Contest (UTPC) is a chapter of the International Collegiate <br/> Programming Contest (ICPC). </p>
            <p className="content"> We put on bi-weekly competitive programming contests on Fridays <br/> in GDC 2.216, everyone is welcomed!</p> 
            <p className="content"> In addition, there is a one-hour
               <a href="https://www.cs.utexas.edu/users/downing/cs104c/"> competitive programming class</a> taught at UT. <br/> Reach out to <a href="https://www.cs.utexas.edu/users/evouga/">
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,7 @@ class Home extends Component {
         <div>
         <h1 className="head">UTPC</h1>
         <a className="subtitle" href="/contact">ICPC & Competitive Programming.</a>
+        <p className="subtitle"> Powered by <a href="https://www.citadel.com/">Citadel & </a><a href="https://www.citadelsecurities.com/">Citadel Securities.</a></p> 
         </div> 
     );
   }
