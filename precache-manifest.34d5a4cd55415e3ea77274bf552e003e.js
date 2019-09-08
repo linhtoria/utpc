853c41d7365faefd03492248ffd0c458c4ceffff
Victoria Nguyen <nguyen.victoriais@gmail.com>
2019-09-07 23:41:49 -0500

    Updates

---
+++ precache-manifest.34d5a4cd55415e3ea77274bf552e003e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c416e4b6a836f346598ca6ce4af2c38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23368158c90c018fff8c",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "7cbebf7ed23d6d838051",
+    "url": "/static/css/main.ff12c2c9.chunk.css"
+  },
+  {
+    "revision": "23368158c90c018fff8c",
+    "url": "/static/js/2.3c37bc18.chunk.js"
+  },
+  {
+    "revision": "7cbebf7ed23d6d838051",
+    "url": "/static/js/main.d1883db3.chunk.js"
+  },
+  {
+    "revision": "0f2503e61232a9f8f67f",
+    "url": "/static/js/runtime~main.810e4dd5.js"
+  }
+]);
